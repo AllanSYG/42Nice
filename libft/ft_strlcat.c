42808d862d2f8acb50417a6963c18aba8e44f397
Allan Sayag <allansayag@hotmail.fr>
2025-04-01 21:53:41 +0200

    strlcpy

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -1,27 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/01 16:21:59 by marvin            #+#    #+#             */
-/*   Updated: 2025/04/01 16:21:59 by marvin           ###   ########.fr       */
+/*   Created: 2025/04/01 16:47:22 by marvin            #+#    #+#             */
+/*   Updated: 2025/04/01 16:47:22 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_strlcpy(char *dst, const char *src, int size)
+int ft_strlcat (char *dst, const char *src, int size)
 {
     int i;
+    int j;
+    int dst_len;
+    int src_len;
 
     i = 0;
-    if (size == 0)
-        return (0);
-    while (src[i] && i < size - 1)
+    j = 0;
+    dst_len = 0;
+    src_len = 0;
+    while (dst[dst_len])
+        dst_len++;
+    while (src[src_len])
+        src_len++;
+    if (size <= dst_len)
+        return (size + src_len);
+    while (src[j] && j < size - dst_len - 1)
     {
-        dst[i] = src[i];
-        i++;
+        dst[dst_len + j] = src[j];
+        j++;
     }
-    dst[i] = '\0';
-    return (i);
+    dst[dst_len + j] = '\0';
+    return (dst_len + src_len);
 }
