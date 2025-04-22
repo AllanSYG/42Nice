e436dfb02f034b6ff5d900f984d3755fe1f57cd6
Allan Sayag <allansayag@hotmail.fr>
2025-04-23 01:40:38 +0200

    get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,7 +10,138 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*get_next_line(int fd)
-{
+#include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
 
+static char *append_to_buffer(char *buffer, char *temp)
+{
+    char    *new_buffer;
+    
+    if (!buffer)
+    {
+        buffer = ft_strjoin("", temp);
+        if (!buffer)
+            return (NULL);
+    }
+    else
+    {
+        new_buffer = ft_strjoin(buffer, temp);
+        free(buffer);
+        if (!new_buffer)
+            return (NULL);
+        buffer = new_buffer;
+    }
+    return (buffer);
 }
+
+static char *read_to_buffer(int fd, char *buffer)
+{
+    char    *temp;
+    ssize_t bytes_read;
+
+    temp = malloc((BUFFER_SIZE + 1) * sizeof(char));
+    if (!temp)
+        return (NULL);
+    
+    bytes_read = 1;
+    while (bytes_read > 0)
+    {
+        bytes_read = read(fd, temp, BUFFER_SIZE);
+        if (bytes_read < 0)
+        {
+            free(temp);
+            return (NULL);
+        }
+        temp[bytes_read] = '\0';
+        buffer = append_to_buffer(buffer, temp);
+        if (!buffer)
+        {
+            free(temp);
+            return (NULL);
+        }
+    }
+    free(temp);
+    return (buffer);
+}
+
+static char *extract_line(char *buffer)
+{
+    char    *line;
+    size_t  len;
+
+    if (!buffer || !*buffer)
+        return (NULL);
+    
+    len = 0;
+    while (buffer[len] && buffer[len] != '\n')
+        len++;
+    
+    line = malloc((len + 2) * sizeof(char));  // +2 to include '\n' and '\0'
+    if (!line)
+        return (NULL);
+    
+    ft_strlcpy(line, buffer, len + 2);  // Copy the line including '\n'
+    return (line);
+}
+
+static char *update_buffer(char *buffer)
+{
+    char    *new_buffer;
+    size_t  i;
+
+    i = 0;
+    while (buffer[i] && buffer[i] != '\n')
+        i++;
+    
+    if (buffer[i] == '\0')  // No newline found
+    {
+        free(buffer);
+        return (NULL);
+    }
+    
+    if (buffer[++i] == '\0')  // Only newline found, no data left
+    {
+        free(buffer);
+        return (NULL);
+    }
+    
+    new_buffer = malloc(ft_strlen(buffer + i) + 1);
+    if (!new_buffer)
+    {
+        free(buffer);
+        return (NULL);
+    }
+    
+    ft_strlcpy(new_buffer, buffer + i, ft_strlen(buffer + i) + 1);
+    free(buffer);
+    return (new_buffer);
+}
+
+char *get_next_line(int fd)
+{
+    static char *buffer;
+    char        *line;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    buffer = read_to_buffer(fd, buffer);
+    if (!buffer || buffer[0] == '\0')
+    {
+        free(buffer);
+        buffer = NULL;
+        return (NULL);
+    }
+    
+    line = extract_line(buffer);
+    if (!line)
+    {
+        free(buffer);
+        buffer = NULL;
+        return (NULL);
+    }
+
+    buffer = update_buffer(buffer);
+    return (line);
+}
+
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,83 @@
+#include "get_next_line.h"
+#include <stdlib.h>
+
+unsigned int	ft_strlen(const char *a)
+{
+	int	i;
+
+	i = 0;
+	while (a[i])
+		i++;
+	return (i);
+}
+char *ft_strchr(const char *s, int c)
+{
+    while (*s != (char)c && *s != '\0')
+        s++;
+    if (*s == (char)c)
+        return ((char *)s);
+    return (NULL);
+}
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*ptr;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	i = 0;
+	j = 0;
+	ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!ptr)
+		return (NULL);
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		ptr[i + j] = s2[j];
+		j++;
+	}
+	ptr[i + j] = '\0';
+	return (ptr);
+}
+int	ft_strlcpy(char *dst, const char *src, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (size == 0)
+		return (ft_strlen(src));
+	while (src[i] && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	while (s1[i])
+		i++;
+	str = malloc(i + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
